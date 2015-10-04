b8a40f572fb9c67612384f52a2d658209f5bb033
Tim Huddle <blazhen88@gmail.com>
2015-10-03 21:20:21 -0400

    don't jam repo that doesn't exist on other peoples machines in code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var NativeAdapter = require('edge').func(__dirname + '\\NodeWrapper\\bin\\Debug\\NodeWrapper.dll');
+var NativeAdapter = require('edge').func(__dirname + '\\NodeWrapper\\bin\\Release\\NodeWrapper.dll');
 adapter = null;
 
-NativeAdapter({path: 'C:\\RelayClearance'}, function(error, native) {
+NativeAdapter({path: 'some repo'}, function(error, native) {
   if (error) console.error(error);
 
   adapter = native;
